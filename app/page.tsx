e0156e095cd5d1da4853c76a20adbd2f5a2a92e7
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-09 16:40:46 +0000

    refactor: Update import statement for LoginPage component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+import LoginPage from "@/components/Login";
 
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
+      <LoginPage />
     </div>
   );
 }
