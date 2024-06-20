ab72ad78d3b921426a4a5ddc1b017056b8178888
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-20 18:12:50 +0000

    feat: Add authentication schema for sign-in and sign-up forms

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,14 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { z } from "zod"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const authConnect = (type: string) => z.object({
+  fullName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  // Both
+  email: z.string().email(),
+  password: z.string().min(8),
+})
